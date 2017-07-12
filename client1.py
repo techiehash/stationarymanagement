7d37eb5241bc3faeb0a7ad354f8b2ca467d10cb2
techiehash <techiehash70@gmail.com>
2017-07-12 23:42:49 +0530

    second

---
+++ client1.py
@@ -0,0 +1,164 @@
+import re
+import json
+import requests
+
+input1=raw_input("Enter url")
+while True:
+
+    string = raw_input('>')
+    input = string.split()
+    actions = ['add', 'search', 'buy', 'trans']
+
+
+    if input[0] == actions[0]:
+        string1 = actions[0]
+        matches = re.search(r'(.*)of(.*)', string)
+        group1 = matches.group(1)
+        lwords = re.findall('^\w+(.*)', group1)
+        ss = lwords[0]
+        rspace = ss.strip()
+        group2 = matches.group(2)
+        list1 = []
+        result = group2.split()
+
+        for i in result:
+            num = re.search('[0-9]*', i)
+            matching = num.group()
+
+            if i == matching:
+                i = int(i)
+                list1.append(i)
+            else:
+                list1.append(i)
+
+        d = {}
+        d[rspace] = dict(zip(list1[::2], list1[1::2]))
+        jsondata = json.dumps(d)
+        #url = 'http://127.0.0.1:5000/add'
+        url=input1+'/add'
+        headers = {'content-type': 'application/json'}
+        r = requests.post(url, data=jsondata, headers=headers)
+        print r.content
+
+    elif input[0] == actions[1]:
+        ss = input[1]
+        string1 = actions[1]
+        rspace = ss.strip()
+        res=string.split()
+        length= len(res)
+        if (length <= 2):
+            d = {}
+            d['pname'] = rspace
+            jsondata = json.dumps(d)
+           # url = 'http://127.0.0.1:5000/search'
+            url = input1 + '/search'
+            headers = {'content-type': 'application/json'}
+            r = requests.post(url, data=jsondata, headers=headers)
+            data = r.json()
+            for i in data:
+                for k, v in i.items():
+                    print k, v
+                print "\n"
+
+        else:
+            string1 = actions[1]
+            matches = re.search(r'(.*)of(.*)', string)
+            group1 = matches.group(1)
+            lwords = re.findall('^\w+(.*)', group1)
+            ss = lwords[0]
+            rspace = ss.strip()
+            group2 = matches.group(2)
+            list1 = []
+            result = group2.split()
+
+            for i in result:
+                num = re.search('[0-9]*', i)
+                matching = num.group()
+
+                if i == matching:
+                    i = int(i)
+                    list1.append(i)
+                else:
+                    list1.append(i)
+
+            d = {}
+            d[rspace] = dict(zip(list1[::2], list1[1::2]))
+            jsondata = json.dumps(d)
+            #url = 'http://127.0.0.1:5000/search'
+            url=input1+'/search'
+            headers = {'content-type': 'application/json'}
+            r = requests.post(url, data=jsondata, headers=headers)
+            data=r.json()
+            for i in data:
+                    for k, v in i.items():
+                        print k, v
+                    print "\n"
+
+    elif input[0] == actions[2]:
+        string1 = actions[2]
+        matches = re.search(r'(.*)of(.*)', string)
+        group1 = matches.group(1)
+        lwords = re.findall('^\w+(.*)', group1)
+        ss = lwords[0]
+        rspace = ss.strip()
+        group2 = matches.group(2)
+        list1 = []
+        result = group2.split()
+
+        for i in result:
+            num = re.search('[0-9]*', i)
+            matching = num.group()
+
+            if i == matching:
+                i = int(i)
+                list1.append(i)
+            else:
+                list1.append(i)
+
+        d = {}
+        d[rspace] = dict(zip(list1[::2], list1[1::2]))
+
+        jsondata = json.dumps(d)
+      #  url = 'http://127.0.0.1:5000/buy'
+        url = input1 + '/buy'
+        headers = {'content-type': 'application/json'}
+        r = requests.post(url, data=jsondata, headers=headers)
+        data = r.json()
+        for i in data:
+            for k, v in i.items():
+                print k, v
+            print "\n"
+
+
+    elif input[0] == actions[3]:
+        string1 = actions[3]
+
+        lwords = re.findall('^\w+(.*)', string)
+        rspace = input[0].strip()
+        d = {}
+        #
+        onlydata = lwords[0]
+
+        list2 = onlydata.split()
+
+        d[rspace] = dict(zip(list2[::2], list2[1::2]))
+
+        jsondata = json.dumps(d)
+        # print 'json data is {}'.format(jsondata)
+        #url = 'http://127.0.0.1:5000/money'
+        url = input1 + '/money'
+        headers = {'content-type': 'application/json'}
+        r = requests.post(url, data=jsondata, headers=headers)
+        data=r.json()
+
+        data1 = data[1]
+        list2 = []
+
+        if type(data) == type(list2):
+            for i in data1:
+                for k, v in i.items():
+                    print k, v
+                print "\n"
+            print "total net", data[0]
+        else:
+            print data
---
+++ server1.py
@@ -0,0 +1,112 @@
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+import time
+
+app = Flask(__name__)
+connection = MongoClient()
+db = connection.inventory #database name.
+collection = db['products'] # collection name.
+collection1 = db['income']    # collection name
+
+@app.route('/add',methods=['POST'])
+def add():
+      jsonadddata=request.get_json()
+      d = {}
+      for key, values in jsonadddata.items():
+          d['pname'] = key
+          for k, v in values.items():
+              d[k] = v
+      quantity=d['qty']
+      del d['qty']
+      if collection.find(d,{"_id":0}).count()>=1:
+        d[u'qty']=quantity
+        collection.update(d,{"$inc":{"qty":d['qty']}})
+        result ="successfully added"
+        return jsonify(result)
+      else:
+        d[u'qty'] = quantity
+        collection.insert(d)
+        result = "successfully added"
+        return jsonify(result)
+
+@app.route('/search',methods=['POST'])
+def search():
+     data = request.get_json()
+    # list1=['color','company','price','qty']
+    # list2=['company','pages','type','qty','price']
+
+     print data
+     now =  'company'  not in [x for v in data.values() for x in v]
+    # now1 = list2   not in [x for v in data.values() for x in v]
+
+     print now
+   #  print now1
+     if now==True:
+            print "if block"
+            result = list(collection.find(data,{"_id":0}))
+     else:
+            print "else block"
+            d = {}
+            for key,values in data.items():
+                d['pname'] = key
+            for k, v in values.items():
+                d[k] = v
+                result=list(collection.find(d,{"_id":0}))
+     return jsonify(result)
+
+@app.route('/buy',methods=['POST'])
+def buy():
+    data = request.get_json()
+    d = {}
+    for key, values in data.items():
+        d['pname'] = key
+        for k, v in values.items():
+            d[k] = v
+
+    a = time.strftime('%Y-%m-%d')
+
+    if d['pname'] == "book":
+        s1 = list(collection.find({"type": d['type'],"pname":d['pname'],"company": d['company'],"price":d['price'],"pages":d['pages'],
+                              "qty": {"$gte": d['qty']}}))
+        if s1:
+             collection.update({"pname": d['pname']}, {'$inc': {'qty': -d['qty']}})
+             collection1.insert({"pname": d['pname'], "qty": d['qty'], "price": d['price'] * d['qty'], "date": a})
+
+        else:
+            result =list(collection.find({"pname":d['pname']},{"_id":0}))
+            #return jsonify(result)
+    else:
+        s1=list(collection.find({"company": d['company'],"pname":d['pname'],"color":d['color'],"price":d['price'],
+                              "qty": {"$gte": d['qty']}}))
+        if s1:
+            collection.update({"company": d['company']}, {'$inc': {'qty': -d['qty']}})
+            collection1.insert({"pname": d['pname'], "qty": d['qty'], "price": d['price'] * d['qty'], "date": a})
+            result = {"status": "ok"}
+            #return jsonify(result)
+        else:
+            result =list(collection.find({"pname":d['pname']},{"_id":0}))
+
+    return jsonify(result)
+
+
+@app.route('/money',methods=['POST'])
+def money():
+      f=request.get_json()
+      a = time.strftime('%Y-%m-%d')
+
+      s=f.values()[0]
+
+
+      s1=list(collection1.find(s,{"_id":0}))
+      if s1:
+          prices_sum = 0
+          for i in range(0,len(s1)):
+                prices_sum += s1[i]['price']
+          return jsonify(prices_sum,s1)
+
+      else:
+          result="no transactions today"
+          return jsonify(result)
+
+if __name__ == '__main__':
+   app.run(debug = True)
